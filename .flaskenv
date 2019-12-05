4519eef46f55be514058ea7674fdf4e2271cd0db
Caio Martinelli <caio.martinelli@hotmail.com>
2019-12-05 02:02:52 -0300

    Add table

--- .flaskenv
+++ .flaskenv
@@ -1,1 +1,2 @@
-FLASK_APP=microblog.py
+FLASK_APP=dashboard.py
+FLASK_ENV=development
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,14 @@
+from flask import render_template
 from app import app
+import pandas as pd
+
+data = pd.read_csv('data/predicoes.csv')
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    params = {
+        'fila': 'Conta Corrente',
+        'data': data
+    }
+    return render_template('index.html', title='Home', params=params)
